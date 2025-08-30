492b5c6735a7338d5096ac10bc35ba7f95af8493
Long Đặng Hữu <danghuulong394@gmail.com>
2025-08-30 20:48:00 +0700

    feat: implement provider logic for home, databank and more page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/home.dart';
-import 'package:weather_app/login.dart';
-import 'package:weather_app/onboarding.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/rootpage.dart';
+import 'providers/tab_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(create: (_) => TabProvider(), child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false, home: Home());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RootPage(),
+    );
   }
 }
