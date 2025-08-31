a62b00c8898b7451f4abc9d6ee3cb91dba9280a8
Long Đặng Hữu <danghuulong394@gmail.com>
2025-08-31 18:51:26 +0700

    feat: implement more page UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:weather_app/onboarding.dart';
 import 'package:weather_app/rootpage.dart';
 import 'providers/tab_provider.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RootPage(),
+      home: Onboarding(),
     );
   }
 }
